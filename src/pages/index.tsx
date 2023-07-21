bdfc4f8de51ebd0760aac7a01022a33195ab352a
Ash Johns <mail@ashpjohns.com>
2023-07-21 17:58:19 -0400

    add font to header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,7 +24,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <header className={styles.hero}>
+      <header className={`${styles.hero} ${inter.className}`}>
         <Link href="/" className={`${styles.title} ${viaoda.className}`}>
           <h1>Cracking Good Gifts</h1>
         </Link>
